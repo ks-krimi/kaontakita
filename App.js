5016768fa615641dc582034c53fd7ba0b73d367b
Ks Krimi <ny.kalash@gmail.com>
2022-05-24 22:15:14 +0200

    chore: implemante a global state context using useContext API

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import GlobalProvider from './src/context/Provider';
 import AppNavContainer from './src/navigations';
 
 const App = () => {
-  return <AppNavContainer />;
+  return (
+    <GlobalProvider>
+      <AppNavContainer />
+    </GlobalProvider>
+  );
 };
 
 export default App;
--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import DrawerNavigator from './DrawerNavigator';
 import AuthNavigator from './AuthNavigator';
+import {GlobalContext} from '../context/Provider';
 
 const AppNavContainer = () => {
-  const isLoggedIn = true;
+  const {
+    authState: {isLoggedIn},
+  } = useContext(GlobalContext);
+
+  // console.log('state>>>>>', authState.isLoggedIn);
 
   return (
     <NavigationContainer>
