e03dd478624fa54675ec1c51789c1b366b2e1d27
Ks Krimi <ny.kalash@gmail.com>
2022-05-24 12:22:02 +0200

    mon premier commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  useColorScheme,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}></ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
